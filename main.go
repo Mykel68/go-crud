1f173da5522262c37867db7a9a6db8aa22219c11
mykel68 <olowookeremicheal9568@gmail.com>
2024-06-17 15:23:20 +0100

    tested gin

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
