2462040ff68beffe642f00b1bd07ebb893950849
mykel68 <olowookeremicheal9568@gmail.com>
2024-06-17 15:55:09 +0100

    loading .env files

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+    "github.com/joho/godotenv"
+)
 
+func init() {
+		  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+
+}
 
 func main() {
+
+	  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
