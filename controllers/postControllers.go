15a09e8033bc43f31f9cea4d486e537e33d3969a
mykel68 <olowookeremicheal9568@gmail.com>
2024-06-17 18:31:29 +0100

    able to accept request

--- controllers/postControllers.go
+++ controllers/postControllers.go
@@ -1,10 +1,33 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-crud/config"
+	"go-crud/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 
 func PostCreate (c *gin.Context) {
+	// Get data off req body
+	var body struct {
+		Title string
+		Body string
+	}
+
+	c.Bind(&body)
+
+	// create post
+	post := models.Post{Title: body.Title, Body: body.Body}
+	result := config.DB.Create(&post)
+
+if result.Error != nil {
+	c.Status(400)
+	return
+}
+
+// return it
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"post": post,
 		})
 	}
--- main.go
+++ main.go
@@ -16,5 +16,5 @@ config.ConnectDB()
 func main() {
 	r := gin.Default()
 	r.POST("/post", controllers.PostCreate)
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run() 
 }
